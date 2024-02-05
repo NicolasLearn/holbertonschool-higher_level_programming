981e3ab5c57cd5b4d7b3e39a149853e99994d023
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2024-02-05 13:44:02 +0000

    writing file 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module defines a class named Square.
+
+The Square class is a simple class that represents a square.
+
+Class:
+    - Square: This class represents a square.
+    Currently, it doesn't contain anything.
+
+Note:
+    This module does not contain any dependencies
+    or system requirements.
+"""
+class Square:
+    """This class represent a square.
+
+    Currently, it doesn't contain anything.
+    """
+    pass
