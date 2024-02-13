d4c71aa77e5beed8c39253882d15d1306d5cdf27
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2024-02-13 14:03:51 +0000

    add empty method in class file 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Empty class named BaseGeometry
+
+    Public instance method:
+        area(): method not impletmented yet.
+    """
+    def area(self):
+        """method not impletmented yet.
+
+        Raises:
+            Exception: indicated that a method not implemented
+        """
+        raise Exception("area() is not implemented")
