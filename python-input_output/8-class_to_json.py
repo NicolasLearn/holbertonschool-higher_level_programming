94eb2cb1192cfb5f692e211426793077236f79e7
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2024-02-16 11:58:34 +0000

    writing function to file 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyton3
+"""This module defines the function (class_to_json)"""
+
+
+def class_to_json(obj):
+    """Return the dictionnary description of the instance 'obj'
+
+    Args:
+        obj(class): Is an instance of a Class.
+    """
+    return obj.__dict__
