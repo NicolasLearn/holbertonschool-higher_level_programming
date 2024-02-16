b1e1212d6716e816bbd77ad1b5c401efcea0e9bc
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2024-02-16 14:50:18 +0000

    add public method (reload_from_json), file 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This module defines the class Student"""
+
+
+class Student:
+    """Represent a student
+
+    Public method:
+        to_json: Return the dictionnary description of the instance.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Constructor for object Student
+
+        Args:
+            first_name (str): First name of the student instance.
+            last_name (str): Last name of the student instance.
+            age (int): Age of the student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionnary description of the attributes instance
+
+        Args:
+            attrs(list): List of attributes to be return. Default None.
+
+        Returns:
+            dict: If attrs == None, return all attributes.
+                    Else, return attributes that exists.
+        """
+        if attrs is None:
+            return self.__dict__
+        dico = dict()
+        for key in attrs:
+            if key in self.__dict__:
+                dico[key] = self.__dict__.get(key)
+        return dico
+
+    def reload_from_json(self, json):
+        """Update public instance from dict that provides of a JSON file
+
+        Args:
+            json(dict): Contain pairs of key/value that provide of JSON file
+        """
+        for (key, value) in json.items():
+            if key in self.__dict__:
+                setattr(self, key, value)
