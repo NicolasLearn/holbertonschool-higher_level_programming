0d50597755e03ef842c59575fcbde1f340c8df1c
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2024-02-21 14:02:16 +0000

    creates class square, add getter and setter(size), add method(str)

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Defines the class Square that inherit of the class Rectangle(Base)."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class that inherit of the class Rectangle, that represent a Square.
+
+    Args:
+        Rectangle: Parent class.
+
+    Private instance attributes:
+        size, x, y : Handle with getter and setter from the parent class.
+
+    Public method
+        (from this class):
+        __str__(): Return a sentence that describes the Square instance.
+
+        (from parent class):
+        integer_validator(): Check if the given value is right.
+        area(): Return the area of the rectangle.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor of the class. That used the parent constructor.
+
+        Args:
+            size (int): Width and height of the square.
+            x (int): Abscissa of the square. Defaults to 0.
+            y (int): Ordinate of the square. Defaults to 0.
+            id (int): Id of the current object created. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return a sentence that describes the Square instance"""
+        string = "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
+                                               self.id, self.x, self.y,
+                                               self.width)
+        return string
+
+    @property
+    def size(self):
+        """Getter to size attribute"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter to size attribute"""
+        self.width = value
+        self.height = value
