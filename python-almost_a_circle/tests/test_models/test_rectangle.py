745e24a82b4e6288abf37a7f438756c65d24b949
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2024-02-23 11:21:45 +0000

    create directory(test_models)

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Unittest for the module rectangle, class Rectangle"""
+import unittest
+rectangle = __import__('models.rectangle').Rectangle
+
+
+class TestMaxInteger(unittest.TestCase):
+    pass
---
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Unittest for the module square, class Square"""
+import unittest
+square = __import__('models.square').Square
+
+
+class TestMaxInteger(unittest.TestCase):
+    pass
