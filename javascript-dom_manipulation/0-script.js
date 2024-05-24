5d41713bb9f12b6ffddb23dcd5b264a6c5f06f4d
nicolas <nicolas.quoirez@hotmail.fr>
2024-05-24 13:47:40 +0200

    add comment

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,1 +1,3 @@
-document.querySelector('HEADER').style.color = '#FF0000';
+// Select the header element using document.querySelector
+// Update the text color of the header element to red
+document.querySelector('header').style.color = '#FF0000';
